6a3a5d1951f2e69a54ad4788f6e67e6fe228a4ed
helloAmulya <internatomix@gmail.com>
2025-02-16 00:07:17 +0530

    fixed issues

--- src/components/YouTubeDownloader.jsx
+++ src/components/YouTubeDownloader.jsx
@@ -22,14 +22,13 @@ export default function YouTubeDownloader() {
         });
       }, 300);
 
-      const response = await fetch(
-        `http://localhost:5002/video-info?url=${encodeURIComponent(url)}`
-      );
       // const response = await fetch(
-      //   `https://yder-production.up.railway.app/video-info?url=${encodeURIComponent(
-      //     url
-      //   )}`
+      //   `http://localhost:5002/video-info?url=${encodeURIComponent(url)}`
       // );
+      const response = await fetch(
+        `https://web-production-196f.up.railway.app/video-info?url=${encodeURIComponent(url)}`
+      );
+      
 
       const data = await response.json();
 
